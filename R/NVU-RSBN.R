d6fcf24cf1d1c691ac770cda258ad91f69d14e3a
Theodore Lytras <thlytras@gmail.com>
2016-01-21 10:57:23 +0200

    Added corrNVU_2NAV() and corrNVU_VOR()

--- R/NVU-RSBN.R
+++ R/NVU-RSBN.R
@@ -60,20 +60,11 @@ planNVU <- function(plan, points=NULL, cols=c("fix","fixLat","fixLon"), init.mag
 #'
 #' @export
 corrRSBN <- function(lat1, lon1, lat2, lon2, latB, lonB) {
-  crsAB <- gcb(lat1, lon1, lat2, lon2)
-  crsAD <- gcb(lat1, lon1, latB, lonB)
-  S <- -spDistsN1(cbind(lon1, lat1), cbind(lon2, lat2), longlat=TRUE)
-  A <- crsAB-crsAD
-  b <- (spDistsN1(cbind(lon1, lat1), cbind(lonB, latB), longlat=TRUE)/1.852) * (pi/(180*60))
-  Zm <- asin(sin(b)*sin(-A*pi/180))
-  Sm <- S + (1.852*60*180/pi) * atan2(sin(b)*cos(A*pi/180),cos(b))
-  Zm <- (1.852*60*180/pi) * Zm
+  # Get orthodromic coordinates Z and S of the beacon, plus S0 (negative leg distance)
+  Bc <- orthoCoords(lat1, lon1, lat2, lon2, latB, lonB)
 
-  #cat(acos(cos(b*pi/180)/cos(Zm)))
-  #cat(atan2(sin(b*pi/180)*cos(A*pi/180),cos(b*pi/180)))
-
-  f <- (Sm - S)/abs(S)
-  d <- (S/1.852)*(pi/(180*60))
+  f <- (Bc["S"] - Bc["S0"])/abs(Bc["S0"])
+  d <- (Bc["S0"]/1.852)*(pi/(180*60))
 
   A <- sin((1-f)*d)/sin(d)
   B <- sin(f*d)/sin(d)
@@ -83,12 +74,12 @@ corrRSBN <- function(lat1, lon1, lat2, lon2, latB, lonB) {
   lat <- atan2(z,sqrt(x^2+y^2))*180/pi
   lon <- atan2(y,x)*180/pi
   YK <- gcb(lat, lon, lat2, lon2)
-  if (Sm>0) YK <- (180 + YK) %% 360
+  if (Bc["S"]>0) YK <- (180 + YK) %% 360
 
   c(
-    Zm = Zm,
-    Sm = Sm,
-    YK = YK
+    Zm = as.numeric(Bc["Z"]),
+    Sm = as.numeric(Bc["S"]),
+    YK = as.numeric(YK)
   )
 }
 
@@ -155,3 +146,174 @@ corrPlanRSBN <- function(plan, bcLeg, cols=c("fix","fixLat","fixLon", "ZPY")) {
   res$beaconName <- gsub(" VORTAC DME", "", res$beaconName)
   res
 }
+
+
+
+#' NVU position correction using VOR
+#'
+#' This function corrects NVU position parameters (Z and S) based on
+#' radial and distance from a VOR
+#'
+#' @param plan A flight plan, as a data.frame. Plans created with \code{\link{planNVU}(..., merge=TRUE)}
+#' are accepted as input. At a minimum, the data frame.should have four columns:
+#' waypoint names, latitudes, longitudes and ZPY (NVU course). The latter is needed to indirectly
+#' identify the NVU legs, which may not correspond 1:1 to the legs of the flight plan (see
+#' \code{points=} in \code{\link{planNVU}}).
+#' @param leg The current leg of the flight plan, whose NVU parameters are to be corrected.
+#' It is specified either as a number or as the initial fix.
+#' @param VOR ID of the VOR to use.If multiple stations match, the correct one is
+#' determined based on proximity to the initial fix of the leg.
+#' @param radialTo Current \emph{inbound} radial (as indicated on the UShDB instrument of the Tu-154).
+#' @param dist Distance in km, as measured by the DME
+#' @param S Along track distance S, as currently indicated in the NVU computer. If supplied,
+#' it must always be negative (as is the NVU convention).
+#' @param cols An optional vector of column names, if these are named differently in \code{plan}
+#' @return Returns the current orthodromic coordinates Z (cross track error) and S (along track
+#' distance) as a named vector.These can be fed into the NVU computer to correct the course of the
+#' aircraft.
+#'
+#' Because S is changing rapidly as the aircraft travels, for convenience if S is supplied as an
+#' argument, an extra element dS is added to the return vector indicating the difference between
+#' actual (corrected) S minus indicated S. Add dS to the current S indication of the NVU to correct it.
+#'
+#' @export
+corrNVU_VOR <- function(plan, leg, VOR, radialTo, dist, S=NA, cols=c("fix","fixLat","fixLon", "ZPY")) {
+  a <- plan[, cols]
+  names(a) <- c("fix","fixLat","fixLon","ZPY")
+  a <- subset(a, !is.na(ZPY)); rownames(a) <- NULL
+  if (is.character(leg)) leg <- match(leg, a[,"fix"])
+  leg <- leg[1]
+
+  VORd <- findFixes(VOR, refPoint=list(lat = a[leg, "fixLat"], lon = a[leg, "fixLon"]))
+  distR <- (dist/1.852) * (pi/(180*60))
+  LatVOR <- VORd$lat*pi/180; LonVOR <- VORd$lon*pi/180
+
+  radial <- ((radialTo + 180 + VORd$dev) %% 360) *pi/180
+
+  posLat <- asin(sin(LatVOR)*cos(distR)+cos(LatVOR)*sin(distR)*cos(radial))
+  dLon <- atan2(sin(radial)*sin(distR)*cos(LatVOR),cos(distR)-sin(LatVOR)*sin(posLat))
+  posLon <- (LonVOR+dLon+pi %% (2*pi)) - pi
+
+  posLat <- posLat*180/pi; posLon <- posLon*180/pi
+
+  res <- orthoCoords(a$fixLat[leg], a$fixLon[leg], a$fixLat[leg+1], a$fixLon[leg+1], posLat, posLon)[1:2]
+
+  if (is.na(S)) {
+    return(res)
+  } else {
+    return(c(res, "dS" = as.numeric(res["S"])-S))
+  }
+}
+
+
+
+#' NVU position correction using 2 navaids (VOR or NDB)
+#'
+#' This function corrects NVU position parameters (Z and S) based on the bearings
+#' to two navaids (VOR or NDB).
+#'
+#' @param plan A flight plan, as a data.frame. Plans created with \code{\link{planNVU}(..., merge=TRUE)}
+#' are accepted as input. At a minimum, the data frame.should have four columns:
+#' waypoint names, latitudes, longitudes and ZPY (NVU course). The latter is needed to indirectly
+#' identify the NVU legs, which may not correspond 1:1 to the legs of the flight plan (see
+#' \code{points=} in \code{\link{planNVU}}).
+#' @param leg The current leg of the flight plan, whose NVU parameters are to be corrected.
+#' It is specified either as a number or as the initial fix.
+#' @param nav1 ID of the first navaid (VOR or NDB) to use.If multiple stations match,
+#' the correct one is determined based on proximity to the initial fix of the leg.
+#' @param bearing1 Bearing to first navaid (in degrees, as indicated on the UShDB instrument
+#' of the Tu-154).
+#' @param nav2 ID of the second navaid (VOR or NDB) to use.If multiple stations match,
+#' the correct one is determined based on proximity to the initial fix of the leg.
+#' @param bearing2 Bearing to second navaid (in degrees, as indicated on the UShDB instrument
+#' of the Tu-154).
+#' @param S Along track distance S, as currently indicated in the NVU computer. If supplied,
+#' it must always be negative (as is the NVU convention).
+#' @param cols An optional vector of column names, if these are named differently in \code{plan}
+#' @return Returns the current orthodromic coordinates Z (cross track error) and S (along track
+#' distance) as a named vector.These can be fed into the NVU computer to correct the course of the
+#' aircraft.
+#'
+#' Because S is changing rapidly as the aircraft travels, for convenience if S is supplied as an
+#' argument, an extra element dS is added to the return vector indicating the difference between
+#' actual (corrected) S minus indicated S. Add dS to the current S indication of the NVU to correct it.
+#'
+#' @export
+corrNVU_2NAV <- function(plan, leg, nav1, bearing1, nav2, bearing2, S=NA, cols=c("fix","fixLat","fixLon", "ZPY")) {
+  a <- plan[, cols]
+  names(a) <- c("fix","fixLat","fixLon","ZPY")
+  a <- subset(a, !is.na(ZPY)); rownames(a) <- NULL
+  if (is.character(leg)) leg <- match(leg, a[,"fix"])
+  leg <- leg[1]
+
+  NAVd1 <- findFixes(nav1, refPoint=list(lat = a[leg, "fixLat"], lon = a[leg, "fixLon"]))
+  NAVd2 <- findFixes(nav2, refPoint=list(lat = a[leg, "fixLat"], lon = a[leg, "fixLon"]))
+
+  lat1 <- NAVd1$lat*pi/180; lon1 <- -NAVd1$lon*pi/180
+  lat2 <- NAVd2$lat*pi/180; lon2 <- -NAVd2$lon*pi/180
+
+  crs13 <- ((bearing1 + 180 + NAVd1$dev) %% 360) *pi/180
+  crs23 <- ((bearing2 + 180 + NAVd2$dev) %% 360) *pi/180
+
+  dst12 <- 2*asin(sqrt((sin((lat1-lat2)/2))^2 + cos(lat1)*cos(lat2)*sin((lon1-lon2)/2)^2))
+  if (sin(lon2-lon1)<0) {
+    crs12 <- acos((sin(lat2)-sin(lat1)*cos(dst12))/(sin(dst12)*cos(lat1)))
+    crs21 <- 2*pi - acos((sin(lat1)-sin(lat2)*cos(dst12))/(sin(dst12)*cos(lat2)))
+  } else {
+    crs12 <- 2*pi - acos((sin(lat2)-sin(lat1)*cos(dst12))/(sin(dst12)*cos(lat1)))
+    crs21 <- acos((sin(lat1)-sin(lat2)*cos(dst12))/(sin(dst12)*cos(lat2)))
+  }
+
+  ang1 <- ((crs13-crs12+pi) %% (2*pi)) - pi
+  ang2 <- ((crs21-crs23+pi) %% (2*pi)) - pi
+
+  if (sin(ang1)==0 && sin(ang2)==0) stop("Infinity of intersections")
+  if (sin(ang1)*sin(ang2)<0) stop("Intersection ambiguous")
+
+  ang1 <- abs(ang1); ang2 <- abs(ang2)
+  ang3 <- acos(-cos(ang1)*cos(ang2) + sin(ang1)*sin(ang2)*cos(dst12))
+  dst13 <- atan2(sin(dst12)*sin(ang1)*sin(ang2), cos(ang2)+cos(ang1)*cos(ang3))
+  lat3 <- asin(sin(lat1)*cos(dst13) + cos(lat1)*sin(dst13)*cos(crs13))
+  dlon <- atan2(sin(crs13)*sin(dst13)*cos(lat1), cos(dst13)-sin(lat1)*sin(lat3))
+  lon3 <- ((lon1-dlon+pi) %% (2*pi)) - pi
+
+  lat3 <- lat3*180/pi; lon3 <- -lon3*180/pi
+
+  res <- orthoCoords(a$fixLat[leg], a$fixLon[leg], a$fixLat[leg+1], a$fixLon[leg+1], lat3, lon3)[1:2]
+
+  if (is.na(S)) {
+    return(res)
+  } else {
+    return(c(res, "dS" = as.numeric(res["S"])-S))
+  }
+}
+
+
+
+#' Convert geographical coordinates to orthodromic coordinates.
+#'
+#' This function is used in NVU calculations and corrections.
+#'
+#' @param lat1 Latitude of departure point (in degrees)
+#' @param lon1 Longitude of departure point (in degrees)
+#' @param lat2 Latitude of destination point (in degrees)
+#' @param lon2 Longitude of destination point (in degrees)
+#' @param latP Latitude of point to be converted to orthodromic coordinates (in degrees)
+#' @param lonP Longitude of point to be converted to orthodromic coordinates (in degrees)
+#' @return Returns a named vector with orthodromic coordinates Z and S (in km),
+#' as well as S0, the negative of the leg distance (which is equivalently
+#' the S coordinate of the departure point).
+#'
+#' @export
+orthoCoords <- function(lat1, lon1, lat2, lon2, latP, lonP) {
+  crsAB <- gcb(lat1, lon1, lat2, lon2)
+  crsAD <- gcb(lat1, lon1, latP, lonP)
+  S0 <- -spDistsN1(cbind(lon1, lat1), cbind(lon2, lat2), longlat=TRUE)
+  A <- crsAB-crsAD
+  b <- (spDistsN1(cbind(lon1, lat1), cbind(lonP, latP), longlat=TRUE)/1.852) * (pi/(180*60))
+  Z <- asin(sin(b)*sin(-A*pi/180))
+  S <- S0 + (1.852*60*180/pi) * atan2(sin(b)*cos(A*pi/180),cos(b))
+  Z <- (1.852*60*180/pi) * Z
+  c("Z"=Z, "S"=S, "S0"=S0)
+}
+
